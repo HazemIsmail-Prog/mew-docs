8ba038875d4ca91c898669283003455e49320c4b
Hazem <hazem.ismail@hotmail.com~>
2025-08-08 11:52:27 +0300

    fix

--- app/Http/Controllers/LetterController.php
+++ app/Http/Controllers/LetterController.php
@@ -109,7 +109,8 @@ class LetterController extends Controller
             'footerImage' => public_path('images/footer.jpeg'),
         ])
         ->withBrowsershot(function (Browsershot $browsershot) {
-            $browsershot->setIncludePath('$PATH:/home/u651499761/.nvm/versions/node/v22.14.0/bin');
+            $browsershot->setNodeBinary('/home/u651499761/.nvm/versions/node/v22.14.0/bin/node')
+            $browsershot->setNpmBinary('/home/u651499761/.nvm/versions/node/v22.14.0/bin/npm');
         })
         ->paperSize(210, 297, 'mm')
         ->name($file_name);
